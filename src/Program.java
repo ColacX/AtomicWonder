8b9b171de8f905a32b2153627648461782333bf2
ColacX <DanielZheng1989@gmail.com>
2019-09-01 14:59:41 +0200

    disabled imageservice

--- src/Program.java
+++ src/Program.java
@@ -2,9 +2,9 @@ public class Program {
     public ImageService ImageService = new ImageService();
 
     public void Run() throws Exception {
-        var bufferedImage = ImageService.Load("RainbowHeart.png");
-        bufferedImage = ImageService.Convert(bufferedImage);
-        ImageService.Save(bufferedImage, "RainbowHeart_converted.png");
+        // var bufferedImage = ImageService.Load("RainbowHeart.png");
+        // bufferedImage = ImageService.Convert(bufferedImage);
+        // ImageService.Save(bufferedImage, "RainbowHeart_converted.png");
         Designer.main(new String[] {});
     }
 
